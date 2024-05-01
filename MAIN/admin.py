bde536316bb9a9dc22aeb392fd48e26c8c2475ef
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-05-02 00:01:53 +0100

    Registered shoe brand in the admin

--- MAIN/admin.py
+++ MAIN/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product,BasePopup, ProductCategory
+from .models import Product,BasePopup, ProductCategory, ShoeBrand
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(BasePopup)
 admin.site.register(ProductCategory)
+admin.site.register(ShoeBrand)
