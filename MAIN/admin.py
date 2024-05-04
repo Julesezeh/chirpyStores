b48013061ff9406116a89ab24d519e97f2100bbf
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-05-04 09:15:34 +0100

    registered the sizes model

--- MAIN/admin.py
+++ MAIN/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product,BasePopup, ProductCategory, ShoeBrand
+from .models import Product,BasePopup, ProductCategory, ShoeBrand, SizeAvailable
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(BasePopup)
 admin.site.register(ProductCategory)
 admin.site.register(ShoeBrand)
+admin.site.register(SizeAvailable)
