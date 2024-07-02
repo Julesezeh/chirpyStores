356375c2ade156916adc46f50a90b1e2dfb508a5
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-07-02 20:04:30 +0100

    registered the BillingInfo model in the Payment app's admin script

--- Payment/admin.py
+++ Payment/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from . models import Payment, Order, OrderItem
+from . models import Payment, Order, OrderItem, BillingInformation
 
 # Register your models here.
 
 admin.site.register(Payment)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(BillingInformation)
