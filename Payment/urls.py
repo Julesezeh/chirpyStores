459b59adbca37a3a55abc84c6523417d9221bc42
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-07-14 10:08:06 +0100

    process payment route

--- Payment/urls.py
+++ Payment/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-# from .views import initiate_payment, complete_payment
+from .views import process_payment
+
+app_name = "payment"
 
 urlpatterns = [
     # path("",initiate_payment,name="initiate_payment"),
     # path("<str:ref>",complete_payment,name="complete_payment")
-
+    path("process_payment/<str:order_id>",process_payment,name='process_payment')
 
 ]
